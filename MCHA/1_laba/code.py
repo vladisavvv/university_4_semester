15ad9039a1c67d117cfabfba629d59b5a23b827b
Vladislav Vishnevsky <Vladisavvv@gmail.com>
2019-02-11 18:31:27 +0300

    init

---
+++ MCHA/1_laba/code.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from matrix import Matrix
+
+if __name__ == "__main__":
+    a = Matrix([[1, 2], [3, 4]])
---
+++ MCHA/1_laba/matrix.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+class Matrix:
+    def __init__(self, array):
+        self._array = array

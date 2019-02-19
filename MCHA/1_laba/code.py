ccd906e90e94dc4bfa4d7df5f62c39700ec06805
Vladislav Vishnevsky <Vladisavvv@gmail.com>
2019-02-19 03:55:06 +0300

    1laba: end first part

--- MCHA/1_laba/code.py
+++ MCHA/1_laba/code.py
@@ -3,5 +3,15 @@
 
 from matrix import Matrix
 
+def read_SLAE(file_name):
+    with open(file_name, 'r') as file:
+        pass
+
+def main():
+    array = read_SLAE('sample.txt')
+    matrix = Matrix(array)
+
+    print(matrix.gauss())
+
 if __name__ == "__main__":
-    a = Matrix([[1, 2], [3, 4]])
+    main()
--- MCHA/1_laba/matrix.py
+++ MCHA/1_laba/matrix.py
@@ -1,6 +1,33 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from copy import copy
+
 class Matrix:
     def __init__(self, array):
-        self._array = array
+        self._array = copy(array)
+
+        self._n = len(array)
+        self._m = len(array[0]) - 1
+
+    def gauss(self):
+        matrix = Matrix(self._array)
+
+        for i in range(self._n):
+            temp = self._array[i][i]
+
+            self._array[i] = [j / temp for j in self._array[i]]
+
+            for k in range(self._n):
+                if i == k:
+                    continue
+
+                for j in range(self._m + 1):
+                    if j == i:
+                        continue
+
+                    self._array[k][j] -= self._array[k][i] * self._array[i][j]
+
+                self._array[k][i] = 0
+
+        return [self._array[i][-1] for i in range(self._n)]
